bff7f07c9c97d32be2a50f2e97cf7ba33d807fad
dbagyon <dbagyon@szintezis-net.hu>
2025-06-18 11:44:17 +0200

    headers to http responses

--- lambda/upload_handler.py
+++ lambda/upload_handler.py
@@ -38,6 +38,10 @@ def handler(event, context):
 
         return {
             "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True
+            },
             "body": json.dumps({
                 "message": "Image uploaded",
                 "filename": filename,
