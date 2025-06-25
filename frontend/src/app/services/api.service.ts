84295728ac93fed1f7e6454002813e388b86c539
rmezes <rmezes@brighthills.com>
2025-06-25 19:47:30 +0200

    Merge upload service with apiservice.

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from '../../enviroments/env';
+import { FileStoreService } from './file-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private fileStore: FileStoreService
+  ) { }
 
   private getAuthHeaders(): HttpHeaders {
     const token = localStorage.getItem('accessToken');
@@ -68,5 +72,36 @@ export class ApiService {
     );
   }
 
+  uploadPdf(file: File): Observable<any> {
+    return new Observable((observer) => {
+      const reader = new FileReader();
 
+      reader.onload = () => {
+        const base64 = (reader.result as string).split(',')[1];
+
+        const body = {
+          file_base64: base64,
+          filename: file.name
+        };
+
+        this.post(environment.api.uploadUrl, body, 'text/plain').subscribe({
+          next: (res: any) => {
+            if (res?.filename && res?.metadata?.['upload-timestamp']) {
+              this.fileStore.addFile({
+                ...res,
+                createdAt: res?.['upload-timestamp'] ?? new Date().toISOString(),
+                status: 'processing'
+              });
+            }
+            observer.next(res);
+          },
+          error: (err) => observer.error(err),
+          complete: () => observer.complete()
+        });
+      };
+
+      reader.onerror = (error) => observer.error(error);
+      reader.readAsDataURL(file);
+    });
+  }
 }
