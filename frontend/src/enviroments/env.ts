7f0662e2c68f951796019d604d61e05fd34ed0d0
dbagyon <dbagyon@szintezis-net.hu>
2025-06-26 16:50:16 +0200

    feature/bagalyze#45: cognito + api gateway + domain https modifs in env.ts

--- frontend/src/enviroments/env.ts
+++ frontend/src/enviroments/env.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
   cognito: {
-    domain: 'https://bagalyze.auth.eu-central-1.amazoncognito.com',
-    clientId: '6m4ujlmkkrjud4n3fr6ul3tiel',
-    redirectUri: 'http://localhost:4200/callback',
+    domain: 'https://medisplain-test.auth.eu-central-1.amazoncognito.com',
+    clientId: '74sndk1nlo5qf411fdcp0mdh0e',
+    redirectUri: 'https://medisplain.brighthills.cloud/callback',
     responseType: 'code',
   },
   api: {
-    uploadUrl: 'https://poyqoedorf.execute-api.eu-central-1.amazonaws.com/dev/upload',
-    filesUrl: 'https://poyqoedorf.execute-api.eu-central-1.amazonaws.com/dev/files',
-    fileUrl: 'https://poyqoedorf.execute-api.eu-central-1.amazonaws.com/dev/download',
-    userMetaUrl: 'https://poyqoedorf.execute-api.eu-central-1.amazonaws.com/dev/user',
-    fileDetail: 'https://poyqoedorf.execute-api.eu-central-1.amazonaws.com/dev/file-meta',
-    webSocketBaseUrl: 'wss://a9iwmgn508.execute-api.eu-central-1.amazonaws.com/dev/',
+    uploadUrl: 'https://9j3fnzfz98.execute-api.eu-central-1.amazonaws.com/test/upload',
+    filesUrl: 'https://9j3fnzfz98.execute-api.eu-central-1.amazonaws.com/test/files',
+    fileUrl: 'https://9j3fnzfz98.execute-api.eu-central-1.amazonaws.com/test/download',
+    userMetaUrl: 'https://9j3fnzfz98.execute-api.eu-central-1.amazonaws.com/test/user',
+    fileDetail: 'https://9j3fnzfz98.execute-api.eu-central-1.amazonaws.com/test/file-meta',
+    webSocketBaseUrl: 'wss://x4rtfzardl.execute-api.eu-central-1.amazonaws.com/test/',
   }
 };
