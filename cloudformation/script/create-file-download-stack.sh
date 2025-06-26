9452defdb1a377cb32b409b41c14c3da94bd36e3
dbagyon <dbagyon@szintezis-net.hu>
2025-06-26 13:18:11 +0200

    feature/bagalyze#36: file down modif

--- cloudformation/script/create-file-download-stack.sh
+++ cloudformation/script/create-file-download-stack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 aws cloudformation create-stack \
-  --stack-name bagalyze-file-download \
-  --template-body file://14-file-download-stack.yaml \
-  --profile bagalyze \
+  --stack-name medisplain-file-download \
+  --template-body file://13-file-download-stack.yaml \
+  --profile medisplain \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
-      ParameterKey=Environment,ParameterValue=dev \
+      ParameterKey=Environment,ParameterValue=test \
       ParameterKey=CloudFrontKeyPairId,ParameterValue=K2XJMN1YQSUXRJ \
       ParameterKey=CloudFrontPrivateKeySecretName,ParameterValue=arn:aws:secretsmanager:eu-central-1:447923164625:secret:cloudfront-private-key-ghsuW4
