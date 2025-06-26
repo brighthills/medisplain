d2c85d3b65cbc7df2334e3217f2a305b2b1985da
rmezes <rmezes@brighthills.com>
2025-06-26 16:45:16 +0200

    Modify UI.

--- frontend/src/app/components/page-header/page-header.component.ts
+++ frontend/src/app/components/page-header/page-header.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss']
 })
 export class PageHeaderComponent {
   @Input() title: string = '';
   @Input() subtitle: string = '';
+  @Input() createdAt: string = '';
+  @Input() showBack: boolean = false;
+  @Input() backLink: string = '/';
 }
