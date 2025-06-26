6c4683007cdda343ec0fb991673332ba77a818a1
dbagyon <dbagyon@szintezis-net.hu>
2025-06-26 12:07:58 +0200

    feature/bagalyze#36: get user file meta modif

--- cloudformation/script/create-get-user-file-metadata-stack.sh
+++ cloudformation/script/create-get-user-file-metadata-stack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 aws cloudformation create-stack \
-  --stack-name bagalyze-get-user-file-metadata \
+  --stack-name medisplain-get-user-file-metadata \
   --template-body file://08-get-user-file-metadata-stack.yaml \
-  --profile bagalyze \
+  --profile medisplain \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
-      ParameterKey=Environment,ParameterValue=dev
+      ParameterKey=Environment,ParameterValue=test
